e5732929ddea856a245997f84520c2fd71c82f7c
AleksiSaxlund <115921686+AleksiSaxlund@users.noreply.github.com>
2023-12-11 17:35:31 +0200

    asd

--- viikko5/laskin/src/index.py
+++ viikko5/laskin/src/index.py
@@ -16,3 +16,10 @@ def main():
 
 if __name__ == "__main__":
     main()
+def get_all_references_by_user_id(self, user_id):
+        """Returns all citations by the given user id."""
+        references = Reference.query.join(UserReferences_model,
+                                                UserReferences_model.reference_id
+                                                == Reference.id).\
+                                                    filter(UserReferences_model.user_id == user_id,
+                                                           Reference.visible.is_(True)).all()
